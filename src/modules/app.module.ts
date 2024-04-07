cefabf0ad880c9068dc2f3371b7302afea6e77ee
Cam Minh <minhlc98@gmail.com>
2024-04-07 14:14:19 +0700

    [UPDATE] app module path

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserModule } from './modules/user/user.module';
+import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
