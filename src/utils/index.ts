d80acac815608c456316bab55f57db7114ba29ec
Cam Minh <minhlc98@gmail.com>
2024-04-16 08:14:10 +0700

    [UPDATE] Refactor code

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,5 @@
 import { createHash } from "crypto";
 
-const createHashMD5 = (data: string): string => {
+export const createHashMD5 = (data: string): string => {
   return createHash('md5').update(data).digest('hex');
 }
-
-export { createHashMD5 };
