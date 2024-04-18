be7986f3cf689add2f4f63f61fdf17c46e25f6ee
Cam Minh <minhlc98@gmail.com>
2024-04-18 11:31:36 +0700

    enable synchronize

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -16,7 +16,7 @@ import { AuthModule } from './auth/auth.module';
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DATABASE,
       autoLoadEntities: true,
-      synchronize: false,
+      synchronize: true,
     }),
     UserModule,
     UrlModule,
